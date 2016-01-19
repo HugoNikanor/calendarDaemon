6429320abddae56ae52e0a567364379a77182fe3
Hugo <hugo.hornquist@gmail.com>
2016-01-19 22:54:28 +0100

    Added a bunch of text about intended function

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class Main {
 	public static void main( String[] args ) throws IOException {
+		// new thread
 		new directoryMonitor.DirectoryWatcher();
+
+		// use main thred
+		// setup directoryLogger
 	}
 }
