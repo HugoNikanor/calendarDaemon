66509c2d19dcbfcc2a8da9775ca50217626c8ed7
Hugo <hugo.hornquist@gmail.com>
2016-01-21 19:07:49 +0100

    some doc notes

---
+++ src/main/java/eventUploader/EventQue.java
@@ -0,0 +1,4 @@
+// this should write to the eventQue xml file
+//
+// another file in this packet shoul read from that file.
+// This class will notifiy that class whene there is sometihng to read
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,10 +4,24 @@ import java.io.IOException;
 
 public class Main {
 	public static void main( String[] args ) throws IOException {
-		// new thread
 		new directoryMonitor.DirectoryWatcher();
 
-		// use main thred
-		// setup directoryLogger
+		/*
+		main creates a live directoryWatcher and a semi-passive directory logger
+		When either of them detect an event, they add the event to the event que
+		(the event que makes sure that the same event isn't submitted more than once, 
+		this may be a concern if both the live and passive directory wather detect the 
+		change (which they are suposed to do))
+
+		create clientInterface as interface
+		create eventQue
+
+		create directoryLogger( eventQue, interface )
+		create directoryMonitor( eventQue )
+
+
+		alternativly, make the eventQue into a singleton
+
+		*/
 	}
 }
